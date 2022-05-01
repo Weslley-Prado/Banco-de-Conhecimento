ca94788750891bf406260eaaa82ab01b6311945a
Weslley Prado <weslley.prado73@gmail.com>
2022-05-01 20:02:44 -0300

    Update

--- Php/Basic_Module/variables.php
+++ Php/Basic_Module/variables.php
@@ -1,20 +1,28 @@
 <?php
+          //This variable show the name of the server
+          echo "This name server: " .$_SERVER['SERVER_NAME']   ;
+          echo '<br/>';
+          // This variable show where  the respository is localized
+          echo "This is folder server: " .$_SERVER['DOCUMENT_ROOT'];
 
-   /*
-     In this site php.net have the system variables of php; It is important,
-     cause we can use many process as to know o server.
-   */  
-   echo $_SERVER['SERVER_NAME'];
-   echo $_SERVER['DOCUMENT_ROOT'];
-   // We write a variable with $name = value;
-   $name = 'Weslley Prado'; // type of string
-   $age = 27; // type of int number
-   $price = 10.06; // type of double number
-   $bool = true; // type of boolean
-   
-   //Constant
 
-   define('NAME_CONSTANT','VALUE_CONSTANT')
+          $name = 'Weslley Prado';// type of string
+          $age = 27; // type of int number
+          $price = 10.06;// type of double number
+          $bool = true ; // type of boolean
+          //Constant
+          define('NAME_CONSTANT','CONSTANT');
+
+          echo "My name's: "  .$name; 
+          echo "<br/>"; 
+          echo "My age: " .$age; 
+          echo "<br/>";
+          echo "This is a number:" .$price; 
+          echo "<br/>";
+          echo "This is boolean expression: " .$bool; 
+          echo "<br/>";
+          echo "Hi, I can't to be altered, because I am a " .NAME_CONSTANT;
    
-   
+
+
 ?>
