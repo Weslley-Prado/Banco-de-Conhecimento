99c675b9a3d341d8bcb32b0b7dff15b6e2a8f23b
Weslley Prado <weslley.prado73@gmail.com>
2022-06-12 21:31:20 -0300

    Finished class01

--- ReactJs/class01/src/App.js
+++ ReactJs/class01/src/App.js
@@ -1,44 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react';
-import { render } from '@testing-library/react';
+import { Component } from 'react';
 
 // function App() {
 
 class App extends Component {
   //constructor(props){} -> Sem constructor = field
-    //super();
-    //this.handlePClick = this.handlePClick.bind(this); //substuido por uma arrow function
-    state = {
-      name:"Weslley Prado",
-      counter: 0
-    };
+  //super();
+  //this.handlePClick = this.handlePClick.bind(this); //substuido por uma arrow function
+  state = {
+    // name:"Weslley Prado",
+    // counter: 0
+    posts: [
+      {
+        id: 1,
+        title: "The title",
+        body: "The body"
+      },
+      {
+        id: 2,
+        title: "The title",
+        body: "The body"
+      },
+      {
+        id: 3,
+        title: "The title",
+        body: "The body"
+      }
+    ]
+  };
 
 
-handlePClick = () => {
-  // console.log(`<p> Clicado {name}`);
-  this.setState({name: "Prado"})
-  //Quando muda o estado, o render irá ser chamado!
-}
+  // handlePClick = () => {
+  //   // console.log(`<p> Clicado {name}`);
+  //   this.setState({name: "Prado"})
+  //   //Quando muda o estado, o render irá ser chamado!
+  // }
 
-handleAClick = (event) => {
-  event.preventDefault();
-  const { counter } = this.state;
-  //const nextCounter = counter++;
-  this.setState({counter: counter + 1})
-}
+  // handleAClick = (event) => {
+  //   event.preventDefault();
+  //   const { counter } = this.state;
+  //   //const nextCounter = counter++;
+  //   this.setState({counter: counter + 1})
+  // }
 
-render() {
-  //const name = this.state.name;
-  const {name, counter} =  this.state;  // This is descructuring
+  render() {
+    //const name = this.state.name;
+    // const {name, counter} =  this.state;  // This is descructuring
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
 
 
-return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p onClick={() => console.log('<p> clicado')}> */}
-        <p onClick={this.handlePClick}>
+        {/* <p onClick={this.handlePClick}>
                 {name} {counter}
         </p>
         <a
@@ -50,9 +67,16 @@ return (
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+      </header> */}
+        {/* Toda vez que ver o map tem que informar a key  */}
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1 >{post.title}</h1>
+            <p> {post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 export default App;
