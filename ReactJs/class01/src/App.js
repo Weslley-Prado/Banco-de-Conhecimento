f7f877a66dff03040f11833201bce3587f27f4f4
Weslley Prado <weslley.prado73@gmail.com>
2022-06-14 18:43:31 -0300

    Debug

--- ReactJs/class01/src/App.js
+++ ReactJs/class01/src/App.js
@@ -1,96 +1,44 @@
 import './App.css';
 import { Component } from 'react';
-import { PostCard } from './components/PostCard';
+import { loadPosts } from './utils/load-posts';
+import { Posts } from './components/Posts/index';
 
 // function App() {
 
 
 class App extends Component {
-  //constructor(props){} -> Sem constructor = field
-  //super();
-  //this.handlePClick = this.handlePClick.bind(this); //substuido por uma arrow function
+ 
   state = {
-    // name:"Weslley Prado",
-    // counter: 0,
+   
     posts: []
-    //   {
-    //     id: 1,
-    //     title: "The title",
-    //     body: "The body"
-    //   },
-    //   {
-    //     id: 2,
-    //     title: "The title",
-    //     body: "The body"
-    //   },
-    //   {
-    //     id: 3,
-    //     title: "The title",
-    //     body: "The body"
-    //   }
-    // ]
+    
   };
 
 
-  // handlePClick = () => {
-  //   // console.log(`<p> Clicado {name}`);
-  //   this.setState({name: "Prado"})
-  //   //Quando muda o estado, o render irá ser chamado!
-  // }
+ 
 
-  // handleAClick = (event) => {
-  //   event.preventDefault();
-  //   const { counter } = this.state;
-  //   //const nextCounter = counter++;
-  //   this.setState({counter: counter + 1})
-  // }
-
-  //Lifecycle
-
-  timeoutUpdate = null;
-
-  componentDidMount() {
+  async componentDidMount() {
     // this.handleTimeout();  
-    this.loadPosts()
+    await this.loadPosts()
 
   }
 
   loadPosts = async () => {
-   const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
-   const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
-
-   const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-   const postsJson = await posts.json();
-   const photosJson = await photos.json();
-   //Unindo array pelo menor
-   const postsAndPhotos = postsJson.map((post, index) => {
-       return { ...post, cover: photosJson[index].url }
-   });
-
-   this.setState({posts: postsAndPhotos})
-   
-  }
-
-  componentDidUpdate() {
-    // this.handleTimeout();  
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos})
 
   }
 
-  componentWillUnmount() {
-    // clearTimeout(this.timeoutUpdate); //Esse método serve para limpar lixo da página
+  // componentDidUpdate() {
+  //   // this.handleTimeout();  
 
-  }
-
-
-  // handleTimeout = () => {
-  //   const {posts, counter } = this.state;
-  //   posts[0].title = "O titulo mudou"; 
-
-  //  this.timeoutUpdate = setTimeout(() => 
-  //   {this.setState({posts, counter: counter + 1
-  //   })
-  // }, 1000);
   // }
+
+  // componentWillUnmount() {
+  //   // clearTimeout(this.timeoutUpdate); //Esse método serve para limpar lixo da página
+
+  // }
+
 
   render() {
     //const name = this.state.name;
@@ -99,36 +47,7 @@ class App extends Component {
 
     return (
       <section className='container'>     
-         <div className="posts">
-
-
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p onClick={() => console.log('<p> clicado')}> */}
-        {/* <p onClick={this.handlePClick}>
-                {name} {counter}
-        </p>
-        <a
-        onClick={this.handleAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-        {/* Toda vez que ver o map tem que informar a key  */}
-        {posts.map(post => (
-          <PostCard 
-          key={post.id}
-          title={post.title}
-          body={post.body}
-          cover={post.cover}
-          id={post.id}
-          />
-        ))}
-      </div>
+        <Posts posts={posts}/>
       </section>
 
     );
